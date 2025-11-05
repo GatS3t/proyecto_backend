62a2a09d11d1a8002612144126172dc26f7e87c0
GatS3t <nelson.rios06@inacapmail.cl>
2025-11-05 18:29:54 -0300

    skjgsldgf

---
+++ aplicacion1/models.py
@@ -0,0 +1,147 @@
+from django.db import models
+import datetime
+from rutificador import Rut
+from django.core.exceptions import ValidationError
+
+
+ahora = datetime.datetime.now
+
+# Create your models here.
+
+
+
+def ValidarRut(rut):
+    try:
+        rut_valido = Rut(rut)
+    except:
+        raise ValidationError('Digito verificador NO corresponde.')
+
+def ValidarMayoriaEdad(fecha_nacimiento):
+    hoy = datetime.date.today() 
+    edad = hoy.year - fecha_nacimiento.year
+    
+    #Resta un año si aún no ha cumplido años este año
+    if (hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day):
+        edad -= 1
+    if edad < 18:   
+        raise ValidationError('Debe ser mayor de edad...')
+
+class Nacionalidad(models.Model):
+    pais = models.CharField(max_length=50, blank=False)
+    nacionalidad = models.CharField(max_length=50, blank=False)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nacionalidad
+
+
+class Autor(models.Model):
+    id_nacionalidad = models.ForeignKey(
+        Nacionalidad, on_delete=models.CASCADE, blank=True)
+    nombre = models.CharField(max_length=250, blank=False)
+    pseudonimo = models.CharField(max_length=50, blank=True)
+    biografia = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.pseudonimo != '':
+            return self.pseudonimo
+        else:
+            return self.nombre
+
+
+class Comuna(models.Model):
+    codigo_comuna = models.CharField(max_length=5, blank=False)
+    nombre_comuna = models.CharField(max_length=50, blank=False)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+class Direccion(models.Model):
+    id_comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE, blank=False)
+    calle = models.CharField(max_length=50, blank=False, default='')
+    numero = models.CharField(max_length=10, blank=False, default='')
+    departamento = models.CharField(max_length=10, blank=True)
+    detalles = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comuna: {self.id_comuna}, Calle: {self.calle}, N°: {self.numero}'
+
+
+class Biblioteca(models.Model):
+    id_direccion = models.ForeignKey(
+        Direccion, on_delete=models.CASCADE, null=True)
+    nombre_biblioteca = models.CharField(max_length=100, blank=False)
+    web = models.CharField(max_length=255, blank=True)
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre_biblioteca
+
+
+class Lector(models.Model):
+    id_biblioteca = models.ForeignKey(
+        Biblioteca, on_delete=models.CASCADE, blank=False)
+    id_direccion = models.ForeignKey(
+        Direccion, on_delete=models.CASCADE, blank=True)
+    rut_lector = models.CharField(max_length=12, unique=True, validators=[ValidarRut])
+    # digito_verificador = models.CharField(max_length=1, blank=False)
+    nombre_lector = models.CharField(max_length=255, blank=False)
+    correo_lector = models.CharField(max_length=255, blank=True)
+    fecha_nacimiento = models.DateField(blank=True, validators=[ValidarMayoriaEdad])
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class TipoCategoria(models.Model):
+    tipo_categoria = models.CharField(max_length=50, blank=False)
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Categoria(models.Model):
+    id_tipo_categoria = models.ForeignKey(
+        TipoCategoria, on_delete=models.CASCADE, blank=False)
+    categoria = models.CharField(max_length=100, blank=False)
+    descripcion = models.CharField(max_length=255, blank=True)
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.categoria
+
+
+
+class Libro(models.Model):
+    id_biblioteca = models.ForeignKey(
+        Biblioteca, on_delete=models.CASCADE, blank=False)
+    id_categoria = models.ForeignKey(
+        Categoria, on_delete=models.CASCADE, blank=True)
+    id_autor = models.ForeignKey(Autor, on_delete=models.CASCADE, blank=False)
+    titulo = models.CharField(max_length=255, blank=False)
+    paginas = models.IntegerField(blank=False)
+    copias = models.IntegerField(blank=False)
+    ubicacion = models.CharField(max_length=255, blank=False)
+    fisico = models.BooleanField(default=True)
+    habilitado = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=ahora)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Prestamo(models.Model):
+    id_libro = models.ForeignKey(Libro, on_delete=models.CASCADE, blank=False)
+    id_lector = models.ForeignKey(Lector, on_delete=models.CASCADE, blank=False)
+    fecha_prestamo = models.DateTimeField(auto_now_add=True)
+    fecha_devolucion = models.DateField(blank=True)
+    fecha_retorno = models.DateTimeField(blank=True)
